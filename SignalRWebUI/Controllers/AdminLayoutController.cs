3921ddbd9515a151b16b895de26cf69f5cffd303
emir adıyaman <emiradymn4444@gmail.com>
2024-12-14 01:36:42 +0300

    Tema nın eklenmesi. ViewComponent ile dinamikleştirildi.

---
+++ SignalRWebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers;
+
+public class AdminLayoutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers;
+
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
